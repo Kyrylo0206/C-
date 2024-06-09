c9e8886d4c88ef0be8b781b4f2e3c5b6ea54b7f7
Kyrylo Shykhalov <kshykhalov@kse.org.ua>
2024-06-09 17:03:01 +0300

    add classes and functioin

--- Task.cpp
+++ Task.cpp
@@ -173,3 +173,194 @@ void TextStorage::ensureCapacity() {
     }
 }
 
+class Command {
+public:
+    virtual void execute() = 0;
+    virtual void undo() = 0;
+    virtual ~Command() = default;
+};
+
+class InsertCommand : public Command {
+public:
+    InsertCommand(TextStorage* storage, int line, int index, const char* str);
+    void execute() override;
+    void undo() override;
+
+private:
+    TextStorage* storage;
+    int line;
+    int index;
+    char* str;
+};
+
+InsertCommand::InsertCommand(TextStorage* storage, int line, int index, const char* str)
+        : storage(storage), line(line), index(index) {
+    this->str = (char*)malloc((strlen(str) + 1) * sizeof(char));
+    strcpy(this->str, str);
+}
+
+void InsertCommand::execute() {
+    storage->insertText(line, index, str);
+}
+
+void InsertCommand::undo() {
+    storage->deleteText(line, index, strlen(str));
+    free(str);
+}
+
+class DeleteCommand : public Command {
+public:
+    DeleteCommand(TextStorage* storage, int line, int index, int length);
+    void execute() override;
+    void undo() override;
+
+private:
+    TextStorage* storage;
+    int line;
+    int index;
+    int length;
+    char* deletedText;
+};
+
+DeleteCommand::DeleteCommand(TextStorage* storage, int line, int index, int length)
+        : storage(storage), line(line), index(index), length(length) {
+    deletedText = nullptr;
+}
+
+void DeleteCommand::execute() {
+    char* lineText = storage->getText(line);
+    if (lineText) {
+        deletedText = (char*)malloc((length + 1) * sizeof(char));
+        strncpy(deletedText, &lineText[index], length);
+        deletedText[length] = '\0';
+        storage->deleteText(line, index, length);
+    }
+}
+
+void DeleteCommand::undo() {
+    if (deletedText) {
+        storage->insertText(line, index, deletedText);
+        free(deletedText);
+    }
+}
+
+class TextEditor {
+public:
+    TextEditor();
+    ~TextEditor();
+    void run();
+
+private:
+    TextStorage* textStorage;
+    std::stack<Command*> undoStack;
+    std::stack<Command*> redoStack;
+
+    void handleCommand(int command);
+    void appendText(const char* newText);
+    void newLine();
+    void saveToFile(const char* filename);
+    void loadFromFile(const char* filename);
+    void printText();
+    void insertText(int line, int index, const char* str);
+    void deleteText(int line, int index, int length);
+    void searchText(const char* str);
+    void clearConsole();
+    void freeTextStorage();
+    void undo();
+    void redo();
+};
+
+TextEditor::TextEditor() {
+    textStorage = new TextStorage();
+}
+
+TextEditor::~TextEditor() {
+    delete textStorage;
+    while (!undoStack.empty()) {
+        delete undoStack.top();
+        undoStack.pop();
+    }
+    while (!redoStack.empty()) {
+        delete redoStack.top();
+        redoStack.pop();
+    }
+}
+
+void TextEditor::run() {
+    int user_input;
+    char inputBuffer[BUFFER_SIZE];
+    int line, index, length;
+    char filename[144];
+
+    while (true) {
+        std::cout << "\nChoose the command from 1 to 11:\n ";
+        std::cin >> user_input;
+        std::cin.ignore();
+        handleCommand(user_input);
+    }
+}
+
+void TextEditor::handleCommand(int command) {
+    int line, index, length;
+    char inputBuffer[BUFFER_SIZE];
+    char filename[144];
+
+    switch (command) {
+        case 1:
+            std::cout << "1. Enter text to append\n";
+            std::cin.getline(inputBuffer, BUFFER_SIZE);
+            appendText(inputBuffer);
+            break;
+        case 2:
+            newLine();
+            break;
+        case 3:
+            std::cout << "3. Enter the file name for saving:\n ";
+            std::cin.getline(filename, 144);
+            saveToFile(filename);
+            break;
+        case 4:
+            std::cout << "4. Enter the file name for loading:\n ";
+            std::cin.getline(filename, 144);
+            loadFromFile(filename);
+            break;
+        case 5:
+            printText();
+            break;
+        case 6:
+            std::cout << "6. Choose line and index:\n ";
+            std::cin >> line >> index;
+            std::cin.ignore();
+            std::cout << "Enter text to insert: ";
+            std::cin.getline(inputBuffer, BUFFER_SIZE);
+            insertText(line, index, inputBuffer);
+            break;
+        case 7:
+            std::cout << "7. Search text position\n";
+            std::cin.getline(inputBuffer, BUFFER_SIZE);
+            searchText(inputBuffer);
+            break;
+        case 8:
+            clearConsole();
+            break;
+        case 9:
+            freeTextStorage();
+            exit(0);
+        case 10:
+            undo();
+            break;
+        case 11:
+            redo();
+            break;
+        case 12:
+            std::cout << "8. Choose line, index and number of symbols:\n ";
+            std::cin >> line >> index >> length;
+            std::cin.ignore();
+            deleteText(line, index, length);
+            break;
+        default:
+            std::cout << "Invalid command.\n";
+            break;
+    }
+}
+
